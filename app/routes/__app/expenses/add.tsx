6d0cf9646e1990c60d045c8367472ad814ba27e5
Alex Sinfarosa <alexsinfarosa@users.noreply.github.com>
2022-11-12 19:52:43 +0100

    sync schema with sqlite

--- app/routes/__app/expenses/add.tsx
+++ app/routes/__app/expenses/add.tsx
@@ -1,6 +1,14 @@
 import {useNavigate} from '@remix-run/react'
 import ExpenseForm from '~/components/expenses/ExpenseForm'
 import Modal from '~/components/util/Modal'
+import type {ActionArgs} from '@remix-run/node'
+import {redirect} from '@remix-run/node'
+
+export async function action({request}: ActionArgs) {
+  const formData = await request.formData()
+  console.log(formData)
+  return redirect(``)
+}
 
 export default function AddExpensesPage() {
   const navigate = useNavigate()
